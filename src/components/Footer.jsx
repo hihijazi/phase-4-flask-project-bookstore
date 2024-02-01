cd18bde42eaa33de699e374a4f9619c4571014f2
hadil <hihijazi@gmail.com>
2024-02-01 14:15:14 -0500

    added footer details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer>
             <div>
                 <p>Bookstore Management System</p>
-                <p>&copy; 2024 All rights reserved.</p>
+                <p>&copy; App Completed by Hadil Hijazi & Melissa Velasquez Greene         2024 All rights reserved.</p>
             </div>
         </footer>
     );
