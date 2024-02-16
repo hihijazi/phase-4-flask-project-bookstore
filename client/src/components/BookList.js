ec626927b07c70e2df8a75245c489fd45b57f1bf
Igor Rakush <irakush@aol.com>
2024-02-16 09:34:41 -0500

    fix some bugs

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,48 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const BookList = () => {
-    const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
 
-    useEffect(() => {
-        fetchBooks();
-    }, []);
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
-    const fetchBooks = async () => {
-        try {
-            const response = await fetch('http://127.0.0.1:5000/books/');
-            if (!response.ok) {
-                throw new Error('Failed to fetch books');
-            }
-            const data = await response.json();
-            setBooks(data);
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:5000/books");
+      if (!response.ok) {
+        throw new Error("Failed to fetch books");
+      }
+      const data = await response.json();
+      setBooks(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-    const suggestedBooks = Array.from({ length: 5 }, (_, index) => ({
-        title: `Book ${index + 1}`,
-        author: `Author ${index + 1}`,
-        id: index + 1,
-    }));
+  const suggestedBooks = Array.from({ length: 5 }, (_, index) => ({
+    title: `Book ${index + 1}`,
+    author: `Author ${index + 1}`,
+    id: index + 1,
+  }));
 
-    const allBooks = [...books, ...suggestedBooks];
+  const allBooks = [...books, ...suggestedBooks];
 
-    return (
-        <div>
-            <h2> </h2>
-            <div>
-                {allBooks.map(book => (
-                    <div key={book.id} style={{ marginLeft: '10px', marginBottom: '10px' }}>
-                        <Link to={`/books/${book.id}`}>
-                            {book.title} by {book.author}
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2> </h2>
+      <div>
+        {books.map((book) => (
+          <div
+            key={book.id}
+            style={{ marginLeft: "10px", marginBottom: "10px" }}
+          >
+            <Link to={`/books/${book.id}`}>
+              {book.title} by {book.author}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BookList;
